da6ebf22036f32d7dab4953c9b00ac3a43988f95
Nicklaspoke <Nicklas.poke@gmail.com>
2021-03-18 16:16:55 +0100

    Initial commit

---
+++ server/src/main/java/com/jobopertunity/jobopertunity/JobOpertunityApplication.java
@@ -0,0 +1,13 @@
+package com.jobopertunity.jobopertunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobOpertunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobOpertunityApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/jobopertunity/jobopertunity/JobOpertunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobopertunity.jobopertunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobOpertunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
